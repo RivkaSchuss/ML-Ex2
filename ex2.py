5ed9064f977de17b69dc24d1475957af64363317
avihayarzuan <avihayarzuan@gmail.com>
2019-05-19 22:47:02 +0300

    optimized constants

--- ex2.py
+++ ex2.py
@@ -1,22 +1,24 @@
 import sys
-from random import shuffle, random
-
 import numpy as np
-from sklearn.model_selection import train_test_split
 
-perceptron_lr = 0.0001
+perceptron_lr = 0.01
 percptron_epochs = 500
-svm_lr = 0.0001
+
+svm_lr = 0.01
 svm_lamda = 0.01
 svm_epochs = 100
-pa_lr = 0.0001
-pa_epochs = 1000
+
+pa_epochs = 100
+
+male = 5
+female = 10
+infant = 1
 
 
 def load_file(file_name):
     data = []
     # Sex feature to numerical index dictionary
-    sex_to_index = {'M': -1.5, 'F': 1.5, 'I': 0}
+    sex_to_index = {'M': male, 'F': female, 'I': infant}
     # Read file
     with open(file_name, 'r') as file:
         for line in file:
@@ -33,8 +35,8 @@ def load_labels(data):
     return labels
 
 
-def random_permutation(train_x, train_y):
-    permute = np.random.permutation(train_x.shape[0])
+def random_permutation(train_x, train_y, seed):
+    permute = np.random.RandomState(seed).permutation(train_x.shape[0])
     return train_x[permute], train_y[permute]
 
 
@@ -43,20 +45,17 @@ class Perceptron:
         self.train_x = train_x
         self.train_y = train_y
         self.lr = perceptron_lr
-        # self.eta = eta
-        # self.w = np.zeros((num_of_classes, len(train_x[0])))
         self.w = np.zeros((np.unique(train_y).size, train_x.shape[1]))
         self.train()
 
     def train(self, epochs=percptron_epochs):
-        # self.train_x, self.train_y = np.random.shuffle(self.train_x, self.train_y, random_state=1)
         for e in range(epochs):
             for x, y in zip(self.train_x, self.train_y):
-                # predict
                 y_hat = np.argmax(np.dot(self.w, x))
                 if y != y_hat:
                     self.w[int(y), :] += self.lr * x
                     self.w[y_hat, :] -= self.lr * x
+                    self.lr *= (1 - e / epochs)
 
     def predict(self, test_x):
         predictions = np.zeros(test_x.shape[0])
@@ -70,18 +69,15 @@ class SVM:
         self.train_x = train_x
         self.train_y = train_y
         self.lr = lr
-        # self.eta = eta
         self.lamda = lamda
         self.num_of_classes = num_of_classes
-        self.w = np.random.uniform(-0.5, 0.5, [np.unique(train_y).size, train_x.shape[1]])
-        # self.w = np.zeros((np.unique(train_y).size, train_x.shape[1]))
+        self.w = np.zeros((np.unique(train_y).size, train_x.shape[1]))
         self.train()
 
-    def train(self, epochs=100):
+    def train(self, epochs=svm_epochs):
         eta_lamda = 1 - self.lr * self.lamda
         for e in range(epochs):
             for x, y in zip(self.train_x, self.train_y):
-                # predict
                 y_hat = int(np.argmax(np.dot(self.w, x)))
                 if y != y_hat:
                     self.w[int(y), :] = eta_lamda * self.w[int(y), :] + self.lr * x
@@ -89,14 +85,7 @@ class SVM:
                     for i in range(self.num_of_classes):
                         if i != y and i != y_hat:
                             self.w[i, :] *= eta_lamda
-                    # eta_x = self.eta * x
-                    # self.w[int(y), :] *= eta_lamda
-                    # self.w[int(y), :] += eta_x
-                    # self.w[y_hat, :] *= eta_lamda
-                    # self.w[y_hat, :] -= eta_x
-            # prediction_train = self.predict(self.train_x)
-            # error = np.mean(prediction_train != self.train_y, dtype=np.float64)
-            # print(1 - error)
+                    self.lr *= (1 - (e / epochs))
 
     def predict(self, test_x):
         predictions = np.zeros(test_x.shape[0])
@@ -106,22 +95,20 @@ class SVM:
 
 
 class PA:
-    def __init__(self, train_x, train_y, lr=pa_lr):
+    def __init__(self, train_x, train_y):
         self.train_x = train_x
         self.train_y = train_y
-        self.lr = lr
         self.w = np.zeros((np.unique(train_y).size, train_x.shape[1]))
         self.train()
 
     def train(self, epochs=pa_epochs):
         for e in range(epochs):
             for x, y in zip(self.train_x, self.train_y):
-                # predict
                 y_hat = int(np.argmax(np.dot(self.w, x)))
                 if y != y_hat:
                     t = (max(0, 1 - (np.dot(self.w[int(y)], x)) + (np.dot(self.w[int(y_hat)], x)))) / (
                             np.linalg.norm(x) ** 2)
-                    tx = t * x
+                    tx = t * x * (1 - e / epochs)*0.7
                     self.w[int(y), :] += tx
                     self.w[y_hat, :] -= tx
 
@@ -132,24 +119,22 @@ class PA:
         return predictions
 
 
-def run_perceptron(train_data, train_labels, test_data, test_labels):
+def run_perceptron(train_data, train_labels, test_data, test_labels=[]):
     perceptron = Perceptron(train_data, train_labels)
     prediction_train = perceptron.predict(test_data)
     error = np.mean(prediction_train != test_labels, dtype=np.float64)
 
-    # print("train accuracy: {} %".format(100 - np.mean(np.abs(prediction_train - train_labels)) * 100))
-    # print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_pred_test - Y_test)) * 100))
     return 1 - error, prediction_train
 
 
-def run_svm(train_data, train_labels, test_data, test_labels):
+def run_svm(train_data, train_labels, test_data, test_labels=[]):
     svm = SVM(train_data, train_labels)
     prediction_train = svm.predict(test_data)
     error = np.mean(prediction_train != test_labels, dtype=np.float64)
     return 1 - error, prediction_train
 
 
-def run_pa(train_data, train_labels, test_data, test_labels):
+def run_pa(train_data, train_labels, test_data, test_labels=[]):
     pa = PA(train_data, train_labels)
     prediction_train = pa.predict(test_data)
     error = np.mean(prediction_train != test_labels, dtype=np.float64)
@@ -187,17 +172,11 @@ def load_data(train_data, train_label, test_data):
     train_data = np.genfromtxt(train_data, delimiter=',', dtype="|U5")
     train_label = np.genfromtxt(train_label, delimiter=",")
     test_data = np.genfromtxt(test_data, delimiter=",", dtype="|U5")
-    # test_label = np.genfromtxt(test_label, delimiter=',')
 
     train_data = organize_features(train_data, ['M', 'F', 'I'])
     test_data = organize_features(test_data, ['M', 'F', 'I'])
 
     train_data = normalize(train_data)
-    # train_x, test_x, train_y, test_y = train_test_split(train_data, train_label, test_size=0.2)
-    # np.savetxt('test_x.txt', test_x, fmt='%s', delimiter=',')
-    # np.savetxt('test_y.txt', test_y, fmt='%s', delimiter='\n')
-    # np.savetxt('train_x_n.txt', test_x, fmt='%s', delimiter=',')
-    # np.savetxt('train_y_n.txt', test_x, fmt='%s', delimiter='\n')
     test_data = normalize(test_data)
 
     return train_data, train_label, test_data
@@ -211,23 +190,28 @@ def data_print(perceptron_pred, svm_pred, pa_pred):
 
 def main():
     args = sys.argv
-    train_x, train_y, test_x, test_y = args[1], args[2], args[3], args[4]
-    # train_data, train_labels, test_train = load_data(train_x, train_y, test_x)
+
+    train_x, train_y, test_x = args[1], args[2], args[3]
+    # train_x, train_y, test_x, test_y = args[1], args[2], args[3], args[4]
+    # test_labels = load_labels(test_y)
+
     train_data = load_file(train_x)
     train_labels = load_labels(train_y)
-    train_data, train_labels = random_permutation(train_data, train_labels)
+
     test_data = load_file(test_x)
-    test_labels = load_labels(test_y)
 
-    perceptron_precision, perceptron_predict = run_perceptron(train_data, train_labels, test_data, test_labels)
-    svm_precision, svm_predict = run_svm(train_data, train_labels, test_data, test_labels)
-    pa_precision, pa_predict = run_pa(train_data, train_labels, test_data, test_labels)
+    train_data_per, train_labels_per = random_permutation(train_data, train_labels, 30)
+    perceptron_precision, perceptron_predict = run_perceptron(train_data_per, train_labels_per, test_data)
+    # print('perceptron: ' + str(perceptron_precision))
 
-    print(perceptron_precision)
-    print(svm_precision)
-    print(pa_precision)
+    svm_precision, svm_predict = run_svm(train_data_per, train_labels_per, test_data)
+    # print('svm: ' + str(svm_precision))
 
-    # data_print(perceptron_predict, svm_predict, pa_predict)
+    train_data_pa, train_labels_pa = random_permutation(train_data, train_labels, 15)
+    pa_precision, pa_predict = run_pa(train_data_pa, train_labels_pa, test_data)
+    # print('pa: ' + str(pa_precision))
+
+    data_print(perceptron_predict, svm_predict, pa_predict)
 
 
 if __name__ == "__main__":
